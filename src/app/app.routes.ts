643dd2f9e0898b1be70d70346b68cbee37737b4c
Richard <daw@richardyuste.com>
2025-10-20 12:17:17 +0200

    Created navabar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'shows', loadComponent: () => import('./pages/shows/shows.component').then(m => m.ShowsComponent) },
+    { path: 'gallery', loadComponent: () => import('./pages/gallery/gallery.component').then(m => m.GalleryComponent) },
+    { path: 'blog', loadComponent: () => import('./pages/blog/blog.component').then(m => m.BlogComponent) },
+    { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' },
+];
---
+++ src/app/pages/shows/shows.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shows',
+  imports: [],
+  templateUrl: './shows.component.html',
+  styleUrl: './shows.component.css'
+})
+export class ShowsComponent {
+
+}
