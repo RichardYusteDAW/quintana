270cb1176c927f315257afafb58ca02821de44c4
Richard <daw@richardyuste.com>
2025-10-22 20:09:21 +0200

    Created CloseUpShowComponent

--- src/app/pages/shows/close-up-show/close-up-show.component.ts
+++ src/app/pages/shows/close-up-show/close-up-show.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-close-up-show',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './close-up-show.component.html',
   styleUrl: './close-up-show.component.css'
 })
-export class CloseUpShowComponent {
-
-}
+export class CloseUpShowComponent { }
