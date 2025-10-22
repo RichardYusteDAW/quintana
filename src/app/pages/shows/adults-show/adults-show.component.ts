cd76cccd9289d1c6f6202007c9f31a941c2e2958
Richard <daw@richardyuste.com>
2025-10-22 20:30:05 +0200

    Created AdultsShowComponent

--- src/app/pages/shows/adults-show/adults-show.component.ts
+++ src/app/pages/shows/adults-show/adults-show.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-adults-show',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './adults-show.component.html',
   styleUrl: './adults-show.component.css'
 })
-export class AdultsShowComponent {
-
-}
+export class AdultsShowComponent { }
