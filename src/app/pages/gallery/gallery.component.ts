fcfe3dbe7f507f081d82a9facdc6c59195f68e2b
Richard <daw@richardyuste.com>
2025-10-23 10:46:42 +0200

    Created GalleryComponent

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent {
-
-}
+export class GalleryComponent { }
