1b161c3ae602dc13bc01399470f8c4be566e8b78
Richard <daw@richardyuste.com>
2025-11-06 17:42:36 +0100

    Updated routes with components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,42 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth/auth.guard';
 
+import { AdminPhotosComponent } from './pages/admin/main/admin-photos/admin-photos.component';
+import { AdminVideosComponent } from './pages/admin/main/admin-videos/admin-videos.component';
+import { AdultsShowComponent } from './pages/user/shows/adults-show/adults-show.component';
+import { BlogComponent } from './pages/user/blog/blog.component';
+import { CloseUpShowComponent } from './pages/user/shows/close-up-show/close-up-show.component';
+import { ContactComponent } from './pages/user/contact/contact.component';
+import { CorporateShowComponent } from './pages/user/shows/corporate-show/corporate-show.component';
+import { FamilyShowComponent } from './pages/user/shows/family-show/family-show.component';
+import { GalleryComponent } from './pages/user/gallery/gallery.component';
+import { HomeComponent } from './pages/user/home/home.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { MainComponent } from './pages/admin/main/main.component';
+import { PhotosComponent } from './pages/user/gallery/photos/photos.component';
+import { ShowsComponent } from './pages/user/shows/shows.component';
+import { VideosComponent } from './pages/user/gallery/videos/videos.component';
+
 export const routes: Routes = [
-    { path: 'home', loadComponent: () => import('./pages/user/home/home.component').then(m => m.HomeComponent) },
+    { path: 'home', component: HomeComponent },
 
-    { path: 'shows', loadComponent: () => import('./pages/user/shows/shows.component').then(m => m.ShowsComponent) },
-    { path: 'shows/family', loadComponent: () => import('./pages/user/shows/family-show/family-show.component').then(m => m.FamilyShowComponent) },
-    { path: 'shows/close-up', loadComponent: () => import('./pages/user/shows/close-up-show/close-up-show.component').then(m => m.CloseUpShowComponent) },
-    { path: 'shows/adults', loadComponent: () => import('./pages/user/shows/adults-show/adults-show.component').then(m => m.AdultsShowComponent) },
-    { path: 'shows/corporate', loadComponent: () => import('./pages/user/shows/corporate-show/corporate-show.component').then(m => m.CorporateShowComponent) },
+    { path: 'shows', component: ShowsComponent },
+    { path: 'shows/family', component: FamilyShowComponent },
+    { path: 'shows/close-up', component: CloseUpShowComponent },
+    { path: 'shows/adults', component: AdultsShowComponent },
+    { path: 'shows/corporate', component: CorporateShowComponent },
 
-    { path: 'gallery', loadComponent: () => import('./pages/user/gallery/gallery.component').then(m => m.GalleryComponent) },
-    { path: 'gallery/photos', loadComponent: () => import('./pages/user/gallery/photos/photos.component').then(m => m.PhotosComponent) },
-    { path: 'gallery/videos', loadComponent: () => import('./pages/user/gallery/videos/videos.component').then(m => m.VideosComponent) },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'gallery/photos', component: PhotosComponent },
+    { path: 'gallery/videos', component: VideosComponent },
 
-    { path: 'blog', loadComponent: () => import('./pages/user/blog/blog.component').then(m => m.BlogComponent) },
-    { path: 'contact', loadComponent: () => import('./pages/user/contact/contact.component').then(m => m.ContactComponent) },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contact', component: ContactComponent },
 
-    { path: 'admin', loadComponent: () => import('./pages/admin/main/main.component').then(m => m.MainComponent), canActivate: [authGuard] },
-    { path: 'admin/login', loadComponent: () => import('./pages/admin/login/login.component').then(m => m.LoginComponent) },
-    { path: 'admin/photos', loadComponent: () => import('./pages/admin/main/admin-photos/admin-photos.component').then(m => m.AdminPhotosComponent), canActivate: [authGuard] },
-    { path: 'admin/videos', loadComponent: () => import('./pages/admin/main/admin-videos/admin-videos.component').then(m => m.AdminVideosComponent), canActivate: [authGuard] },
+    { path: 'admin', component: MainComponent, canActivate: [authGuard] },
+    { path: 'admin/login', component: LoginComponent },
+    { path: 'admin/photos', component: AdminPhotosComponent, canActivate: [authGuard] },
+    { path: 'admin/videos', component: AdminVideosComponent, canActivate: [authGuard] },
 
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home' },
